1bce5c69a33373f3a7bea578914871b01c8c68c2
ScRoLiS <34772324+ScRoLiS@users.noreply.github.com>
2022-04-12 14:59:20 +0600

    add: get books api

---
+++ src/services/api.js
@@ -0,0 +1,21 @@
+export default class API {
+
+  static baseUrl = 'https://bookstore-api-server.herokuapp.com'
+
+  static getBooks = async () => {
+    const req = await fetch(this.baseUrl + '/api/books?populate=image')
+    const { data } = await req.json()
+
+    console.log(data);
+
+    const newData = data.map((item) => {
+      const { id, attributes } = item
+      const { author, name, genre, price, pages, image } = attributes
+      const img = this.baseUrl + image.data.attributes.url
+
+      return { id, author, name, genre, price, pages, img }
+    })
+
+    return newData
+  }
+}
