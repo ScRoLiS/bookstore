2fb33b5d392c1b63694a87ef2befea8ca3ce7fbf
ScRoLiS <34772324+ScRoLiS@users.noreply.github.com>
2022-04-12 17:14:57 +0600

    add: redux store

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { BookCard } from '../components'
+import API from '../services/api'
+import { setBooks } from '../store/actions/bookActions'
 
 const Home = () => {
+  const dispatch = useDispatch()
+  const { books } = useSelector(state => state.books)
+
+  useEffect(() => {
+    if (!books.length)
+      API.getBooks()
+        .then((data) => {
+          dispatch(setBooks(data))
+        })
+  }, [])
+
   return (
-    <div>Home</div>
+    <div className="grid grid-cols-3 gap-4">
+      {books.map((item) => {
+        return <BookCard key={item.id} {...item} />
+      })}
+    </div>
   )
 }
 
