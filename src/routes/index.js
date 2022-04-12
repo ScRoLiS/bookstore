150c84699c334ff10f783e43d1e92b05eb2469e7
ScRoLiS <34772324+ScRoLiS@users.noreply.github.com>
2022-04-12 16:57:46 +0600

    Book created

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,5 @@
 export { default as Home } from './Home'
 export { default as Cart } from './Cart'
 export { default as Login } from './Login'
+export { default as Book } from './Book'
 export { default as PageNotFound } from './PageNotFound'
