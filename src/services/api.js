1b8b7120ab118eb990640606dce8bfb7c9a68db3
ScRoLiS <34772324+ScRoLiS@users.noreply.github.com>
2022-04-14 17:10:37 +0600

    add: get single book function

--- src/services/api.js
+++ src/services/api.js
@@ -3,9 +3,16 @@ export default class API {
   static baseUrl = 'https://bookstore-api-server.herokuapp.com'
 
   static getBooks = async () => {
-    const req = await fetch(this.baseUrl + '/api/books?sort=id:asc&populate=image,genres,authors')
+    const req = await fetch(this.baseUrl + `/api/books?sort=id:asc&populate=image,genres,authors`)
     const { data } = await req.json()
 
     return data
+  }
+
+  static getBook = async (id) => {
+    const req = await fetch(this.baseUrl + `/api/books/${id}?populate=image,genres,authors`)
+    const data = await req.json()
+
+    return { ...data, id: parseInt(data.id) }
   }
 }
