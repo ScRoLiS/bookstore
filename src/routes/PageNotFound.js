151ee8b91ca5808fd045574935189abf9ba665f0
ScRoLiS <34772324+ScRoLiS@users.noreply.github.com>
2022-04-18 22:10:30 +0600

    add: page not fount template

--- src/routes/PageNotFound.js
+++ src/routes/PageNotFound.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import { BiErrorAlt } from 'react-icons/bi'
 
 const PageNotFound = () => {
   return (
-    <div>PageNotFound</div>
+    <div className="bg-white p-4 grow rounded-md shadow-md container flex flex-col items-center justify-center">
+      <div className="text-red-500 flex text-5xl justify-between items-center">
+        <BiErrorAlt className="" /> 404
+      </div>
+      
+      <span className="text-xl font-semibold">Такой страницы нет!</span>
+    </div>
   )
 }
 
