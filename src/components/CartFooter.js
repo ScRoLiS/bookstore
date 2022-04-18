80df14059630827e387ceaf5a7475f999aa7c9b5
ScRoLiS <34772324+ScRoLiS@users.noreply.github.com>
2022-04-18 14:49:32 +0600

    fix: responsive styles

--- src/components/CartFooter.js
+++ src/components/CartFooter.js
@@ -4,10 +4,10 @@ import Button from './Button'
 const CartFooter = ({ items }) => {
   return (
     <div className="mt-4 bg-opacity-90 bg-white rounded-md sticky bottom-0 p-4 flex justify-between items-center">
-      <span className="font-semibold text-xl">Общая сумма: {items.reduce((prev, current) => {
+      <span className="font-semibold text-base md:text-xl">Общая сумма: {items.reduce((prev, current) => {
         return (current.count * current.price) + prev
       }, 0)} тг.</span>
-      <Button className="text-sm w-36">Оформить</Button>
+      <Button className="text-xs md:text-sm md:w-36">Оформить</Button>
     </div>
   )
 }
--- src/routes/Cart.js
+++ src/routes/Cart.js
@@ -26,34 +26,36 @@ const Cart = () => {
 
   if (!cart.length) {
     return (
-      <div className="p-4 rounded-md shadow-md bg-white items-center flex flex-col">
+      <div className="p-4 text-center  rounded-md shadow-md bg-white items-center flex flex-col">
         <span className="font-medium text-lg md:text-2xl">Корзина пуста!</span>
-        <span className="text-center text-sm md:text-base">Перейдите в <Link to="/" className="link">Магазин</Link> для совершения покупок</span>
+        <span className="text-sm md:text-base">Перейдите в <Link to="/" className="link">Магазин</Link> для совершения покупок</span>
       </div>
     )
   }
 
   return (
-    <div className="p-4 rounded-md shadow-md bg-white">
+    <div className="md:p-4 rounded-md shadow-md bg-white">
       <ul className="flex flex-col gap-4 list-none">
         {cart.map((item) => {
           return (
-            <li key={item.id} className="flex gap-4 p-4 shadow-sm rounded-md">
-              <img src={item.image} alt={item.name} className="rounded-md h-40" />
-              <div className="flex flex-col items-start justify-between w-96">
-                <span className="whitespace-nowrap w-full overflow-hidden overflow-ellipsis text-lg font-medium">{item.name}</span>
-                <span>Автор: {item.authors.map((item) => `${item.name} `).join(', ')}</span>
-                <span>Кол-во страниц: {item.pages}</span>
-                <span>Жанр: {item.genres.map((item) => `${item.name}`).join(', ')}</span>
-                <button onClick={() => { handleRemoveFromCart(item.id) }} className="link hover:text-red-400 hover:border-red-400 justify-center">Убрать</button>
+            <li key={item.id} className="grid grid-cols-3 gap-2 p-4 shadow-sm rounded-md">
+              <div className="flex items-start col-span-2 gap-2">
+                <img src={item.image} alt={item.name} className="rounded-md h-28 md:h-40" />
+                <div className="flex flex-col items-start h-full overflow-hidden overflow-ellipsis justify-between text-sm md:text-base">
+                  <span className="whitespace-nowrap w-full overflow-hidden overflow-ellipsis md:text-lg font-medium">{item.name}</span>
+                  <span>Автор: {item.authors.map((item) => `${item.name} `).join(', ')}</span>
+                  <span>Кол-во страниц: {item.pages}</span>
+                  <span>Жанр: {item.genres.map((item) => `${item.name}`).join(', ')}</span>
+                  <button onClick={() => { handleRemoveFromCart(item.id) }} className="link hover:text-red-400 hover:border-red-400 justify-center">Убрать</button>
+                </div>
               </div>
-              <div className="flex xl:grow gap-2 sm:flex-col sm:justify-center sm:items-center xl:flex-row ml-auto">
-                <div className="flex shrink-0 gap-2 xl:ml-32 items-center">
+              <div className="flex gap-2 flex-col justify-center items-center ml-auto lg:ml-0 lg:flex-row">
+                <div className="flex shrink-0 relative gap-7 items-center">
                   <Button type="ghost" disabled={item.count <= 1 ? true : false} onClick={() => { handleDecrementCount(item.id) }}>-</Button>
-                  <span>{item.count}</span>
+                  <span className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">{item.count}</span>
                   <Button type="ghost" onClick={() => { handleIncrementCount(item.id) }}>+</Button>
                 </div>
-                <div className="font-semibold text-2xl lg:ml-auto self-center relative left-1">
+                <div className="font-semibold text-base md:text-2xl lg:ml-auto self-center relative left-1">
                   {item.price * item.count} тг.
                 </div>
               </div>
