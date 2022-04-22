43c4007cf2f13635c7c82ff3a4cd64203ff98890
ScRoLiS <34772324+ScRoLiS@users.noreply.github.com>
2022-04-22 22:33:08 +0600

    fix: styles

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,6 +1,4 @@
 import React, { useEffect } from 'react'
-import { BiErrorAlt } from 'react-icons/bi'
-import { BsExclamationCircle } from 'react-icons/bs'
 import { useDispatch } from 'react-redux'
 import { removeMessage } from '../store/actions/messageActions'
 
@@ -15,24 +13,10 @@ const Message = ({ id, type, message }) => {
     }, 5000)
   }, [])
 
-  if (type === 'notify')
-    return (
-      <div className="max-w-xs p-4 text-white bg-green-300 shadow-md rounded-md flex items-center gap-2">
-        <BsExclamationCircle />
-        {message}
-      </div>
-    )
-
-  if (type === 'error')
-    return (
-      <div className="max-w-xs p-4 text-white bg-red-300 shadow-md rounded-md items-center flex gap-2">
-        <BiErrorAlt />
-        {message}
-      </div>
-    )
-
   return (
-    <div>{message}</div>
+    <div className={`message-${type}`}>
+      {message}
+    </div>
   )
 }
 
