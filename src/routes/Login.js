4b9ab75f0b9e430195ba3a3a7f604c26af6f190b
ScRoLiS <34772324+ScRoLiS@users.noreply.github.com>
2022-04-22 19:12:47 +0600

    add: inputs and handlers

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -1,15 +1,41 @@
-import { Button, Input } from '../components'
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { Button, Input, Spinner } from '../components'
+import React, { useState } from 'react'
+import { Link, Navigate, useNavigate } from 'react-router-dom'
+import { useAuth, useInput } from '../hooks'
+import { useDispatch } from 'react-redux'
+import API from '../services/api'
+import { login } from '../store/actions/userActions'
 
 const Login = () => {
+  const isAuth = useAuth()
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const [isLoading, setLoading] = useState(false)
+  const [email, handleEmail] = useInput('username@mail.com')
+  const [password, handlePassword] = useInput('123456')
+
+  const handleLogin = (e) => {
+    setLoading(true)
+    API.login(email, password)
+      .then((data) => {
+        dispatch(login(data))
+        navigate('/user/profile')
+      })
+      .finally(() => {
+        setLoading(false)
+      })
+  }
+
+  if (isAuth)
+    return <Navigate to="/user/profile" />
+
   return (
     <div className="grow flex items-center justify-center">
       <div className="bg-white rounded-md shadow-md p-8  flex flex-col items-center gap-2 w-96">
         <span className="text-xl">Войти</span>
-        <Input placeholder="Email" type="email" />
-        <Input placeholder="Пароль" type="password" />
-        <Button className="w-full mt-2">Войти</Button>
+        <Input value={email} onChange={handleEmail} placeholder="Email" type="email" />
+        <Input value={password} onChange={handlePassword} placeholder="Пароль" type="password" />
+        <Button disabled={isLoading} onClick={handleLogin} className="w-full mt-2">{isLoading ? <Spinner type="small" /> : 'Войти'}</Button>
         <div className="text-sm flex gap-1 flex-wrap justify-center">
           <span>Нет аккаунта?</span> <Link className="link" to="/user/registration">Регистрация</Link>
         </div>
--- src/routes/Registration.js
+++ src/routes/Registration.js
@@ -1,16 +1,41 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+import { Link, useNavigate } from 'react-router-dom'
 import { Button, Input } from '../components'
+import { useAuth, useInput } from '../hooks'
+import API from '../services/api'
+import { login } from '../store/actions/userActions'
 
 const Registration = () => {
+  const auth = useAuth()
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const [email, handleEmail] = useInput('username@mail.com')
+  const [username, handleUsername] = useInput('username')
+  const [password, handlePassword] = useInput('123456')
+  const [passwordRepeat, handlePasswordRepeat] = useInput('123456')
+
+  const handleRegister = (e) => {
+    API.register(email, username, password)
+      .then((data) => {
+        dispatch(login(data))
+        navigate('/user/profile')
+      })
+  }
+
+  if (auth)
+    navigate('/user/profile')
+
+
   return (
     <div className="grow flex items-center justify-center">
       <div className="bg-white rounded-md shadow-md p-8 flex flex-col items-center gap-2 w-96">
         <span className="text-xl">Регистрация</span>
-        <Input placeholder="Email" type="email" />
-        <Input placeholder="Пароль" type="password" />
-        <Input placeholder="Повторите пароль" type="password" />
-        <Button className="w-full mt-2">Зарегистрироваться</Button>
+        <Input value={email} onChange={handleEmail} placeholder="Email" type="email" />
+        <Input value={username} onChange={handleUsername} placeholder="Имя пользователя" type="text" />
+        <Input value={password} onChange={handlePassword} placeholder="Пароль" type="password" />
+        <Input value={passwordRepeat} onChange={handlePasswordRepeat} placeholder="Повторите пароль" type="password" />
+        <Button onClick={handleRegister} className="w-full mt-2">Зарегистрироваться</Button>
         <div className="text-sm flex gap-1 flex-wrap justify-center">
           <span>Есть аккаунт?</span> <Link className="link" to="/user/login">Войти</Link>
         </div>
