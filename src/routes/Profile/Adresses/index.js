28bc228ee736ff865cddc5f62eca5d41ca09b969
ScRoLiS <34772324+ScRoLiS@users.noreply.github.com>
2022-04-24 01:20:33 +0600

    fix: little fix

---
+++ src/routes/Profile/Adresses/index.js
@@ -0,0 +1,3 @@
+export {default as Adresses} from './Adresses'
+export {default as AddAdress} from './AddAdress'
+export {default as ViewAdresses} from './ViewAdresses'
--- src/routes/Profile/index.js
+++ src/routes/Profile/index.js
@@ -1,4 +1,3 @@
-export { default as Adresses } from './Adresses'
 export { default as Purchases } from './Purchases'
 export { default as UserProfile } from './UserProfile'
 export { default as Profile } from './Profile'
