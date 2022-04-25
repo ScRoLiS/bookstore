963c363162b5b787e17a0ac4ac1e67e8794e63c4
ScRoLiS <34772324+ScRoLiS@users.noreply.github.com>
2022-04-25 18:25:13 +0600

    fix: remove button

--- src/routes/Profile/Adresses/ViewAdresses.js
+++ src/routes/Profile/Adresses/ViewAdresses.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
-import { Button } from '../../../components'
 
 const ViewAdresses = () => {
   const { adresses } = useSelector(state => state.adresses)
