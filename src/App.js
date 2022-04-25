a8d4c09d51cc8dc0a36533fdd7c76b1efed189db
ScRoLiS <34772324+ScRoLiS@users.noreply.github.com>
2022-04-25 18:23:41 +0600

    add: checking jwt

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
-import { useSelector } from 'react-redux';
-import { Route, Routes } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 import { Footer, Message, Navbar } from './components';
+import { useAuth } from './hooks';
 import { Home, Login, Cart, PageNotFound, Book, Registration } from './routes';
 import { Purchases, UserProfile, Profile } from './routes/Profile';
 import { Adresses, AddAdress, ViewAdresses } from './routes/Profile/Adresses';
 import { Cards, AddCard, ViewCards } from './routes/Profile/Cards'
+import API from './services/api';
+import { setCart } from './store/actions/cartAction';
+import { login, logout } from './store/actions/userActions';
 
 function App() {
+  const isAuth = useAuth()
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const { jwt } = useSelector(state => state.user)
   const { messages } = useSelector(state => state.messages)
+
+  useEffect(() => {
+    if (isAuth)
+      API.getUser(jwt)
+        .then((data) => {
+          const { cart, cards } = data
+          dispatch(login({ user: data, jwt }))
+          dispatch(setCart(cart))
+        })
+        .catch((e) => {
+          console.log(e);
+          dispatch(logout())
+          navigate('/')
+        })
+  }, [])
 
   return (
     <div className="flex flex-col min-h-screen">
