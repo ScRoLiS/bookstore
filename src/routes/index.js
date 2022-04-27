d6482d13c2a27e8e96e165f130ba809283cdaa45
ScRoLiS <34772324+ScRoLiS@users.noreply.github.com>
2022-04-27 15:24:09 +0600

    add: Checkout component

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 export { default as Home } from './Home'
 export { default as Cart } from './Cart'
 export { default as Login } from './Login'
-export { default as Registration } from './Registration'
 export { default as Book } from './Book'
+export { default as Checkout } from './Checkout'
+export { default as Registration } from './Registration'
 export { default as PageNotFound } from './PageNotFound'
