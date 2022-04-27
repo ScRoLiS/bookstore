0ab1c420957423eda21715b1c2b1e2799f611d9c
ScRoLiS <34772324+ScRoLiS@users.noreply.github.com>
2022-04-27 16:48:14 +0600

    add: navigate to checkout

--- src/components/CartFooter.js
+++ src/components/CartFooter.js
@@ -1,13 +1,20 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import Button from './Button'
 
 const CartFooter = ({ items }) => {
+  const navigate = useNavigate()
+
+  const checkoutHandle = () => {
+    navigate('/checkout')
+  }
+
   return (
     <div className="mt-4 bg-opacity-90 bg-white sticky bottom-0 p-4 flex justify-between items-center">
       <span className="font-semibold text-base md:text-xl">Общая сумма: {items.reduce((prev, current) => {
         return (current.count * current.price) + prev
       }, 0)} тг.</span>
-      <Button className="text-xs md:text-sm md:w-36">Оформить</Button>
+      <Button onClick={checkoutHandle} className="text-xs md:text-sm md:w-36">Оформить</Button>
     </div>
   )
 }
