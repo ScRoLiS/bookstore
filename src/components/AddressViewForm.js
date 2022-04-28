febcd9b570aac7235176a603c7c9311e208e2b73
ScRoLiS <34772324+ScRoLiS@users.noreply.github.com>
2022-04-28 16:02:02 +0600

    fix: replace div to h1

--- src/components/AddressViewForm.js
+++ src/components/AddressViewForm.js
@@ -6,7 +6,7 @@ const AddressViewForm = ({ address }) => {
     <div>
       <div className="flex flex-col md:flex-row gap-2 mt-4">
         <div className="flex flex-col grow gap-2">
-          <div className="text-base md:text-xl font-medium">Куда</div>
+          <h1 className="text-base md:text-xl font-medium">Куда</h1>
           <div className="flex flex-col sm:flex-row gap-2">
             <DataRow label="Индекс" value={address.address.whereTo.index} />
             <DataRow label="Страна" value={address.address.whereTo.country} />
@@ -19,7 +19,7 @@ const AddressViewForm = ({ address }) => {
           </div>
         </div>
         <div className="flex flex-col gap-2 grow">
-          <div className="text-base md:text-xl font-medium">Кому</div>
+          <h1 className="text-base md:text-xl font-medium">Кому</h1>
           <DataRow label="Фамилия" value={address.address.whom.secondName} />
           <DataRow label="Имя" value={address.address.whom.firstName} />
           <DataRow label="Отчество" value={address.address.whom.surname} />
