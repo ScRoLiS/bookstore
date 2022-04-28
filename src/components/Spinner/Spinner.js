a52b2f52c0a4f766cc882d0abb7cdf331c83ed6c
ScRoLiS <34772324+ScRoLiS@users.noreply.github.com>
2022-04-28 20:18:15 +0600

    add: medium spinner

--- src/components/Spinner/Spinner.js
+++ src/components/Spinner/Spinner.js
@@ -4,12 +4,19 @@ import './Spinner.css'
 
 const Spinner = ({ type }) => {
 
-  if (type && type === 'small')
+  if (type && type === 'small') {
     return (
       <div className="flex flex-grow justify-center items-center">
         <div className="spinner-small"></div>
       </div >
     )
+  } else if (type && type === 'medium') {
+    return (
+      <div className="flex flex-grow justify-center items-center">
+        <div className="spinner-medium"></div>
+      </div >
+    )
+  }
 
   return (
     <div className="flex flex-grow justify-center items-center">
