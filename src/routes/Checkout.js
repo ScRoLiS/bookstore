3040dea45b07ef136c8370df509aaeb77d015fca
ScRoLiS <34772324+ScRoLiS@users.noreply.github.com>
2022-04-29 18:00:36 +0600

    fix: using arrayUtils

--- src/routes/Checkout.js
+++ src/routes/Checkout.js
@@ -1,25 +1,32 @@
 import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Navigate, useNavigate } from 'react-router-dom'
-import { Button, AddressViewForm, Spinner } from '../components'
+import { Button, AddressViewForm, Spinner, ViewCart } from '../components'
 import { useAuth, useInput } from '../hooks'
 import { setForward } from '../store/actions/appActions'
+import { setPurchases } from '../store/actions/purchaseActions'
 import { addMessage } from '../store/actions/messageActions'
+import { resetCart } from '../store/actions/cartAction'
+import { getElementById } from '../utils/arrayUtils'
+import API from '../services/api'
 
 const Checkout = () => {
   const isAuth = useAuth()
   const dispatch = useDispatch()
   const navigate = useNavigate()
 
-  const [isLoading, setLoading] = useState(false)
-  const [addressSelect, setAddressSelect] = useInput('default')
-  const [cardSelect, setCardSelect] = useInput('default')
-  const [address, setAddress] = useState(null)
   const [card, setCard] = useState(null)
+  const [address, setAddress] = useState(null)
+  const [isLoading, setLoading] = useState(false)
+  const [cardSelect, setCardSelect] = useInput('default')
+  const [addressSelect, setAddressSelect] = useInput('default')
 
   const { cart } = useSelector(state => state.cart)
-  const { addresses } = useSelector(state => state.addresses)
+  const { jwt } = useSelector(state => state.user)
   const { cards } = useSelector(state => state.cards)
+  const { purchases } = useSelector(state => state.purchases)
+  const { addresses } = useSelector(state => state.addresses)
+
 
   const finalPrice = () => {
     return cart.reduce((prev, current) => { return prev + current.count * current.price }, 0)
@@ -35,22 +42,23 @@ const Checkout = () => {
     navigate('/user/addresses/add')
   }
 
-  const getElementById = (array, id) => {
-    id = parseInt(id)
-
-    for (let i = 0; i < array.length; i++) {
-      const element = { ...array[i] };
-      if (element.id === id)
-        return element
-    }
-
-    return null
-  }
-
   const payHandle = () => {
     setLoading(true)
-    dispatch(addMessage(Math.random(), 'success', 'Оплата произведена!'))
-    dispatch(addMessage(Math.random(), 'success', 'Заказ в обработке!'))
+    API.sendPurchases(jwt, [...purchases, { id: Math.round(Math.random() * 10000000), purchase: cart, address, card, status: 'В обработке' }])
+      .then((data) => {
+        dispatch(setPurchases(data.purchases))
+        dispatch(resetCart())
+        dispatch(addMessage(Math.random(), 'success', 'Оплата произведена!'))
+        dispatch(addMessage(Math.random(), 'success', 'Заказ в обработке!'))
+        navigate('/user/purchases')
+      })
+      .catch((e) => {
+        console.log(e);
+        dispatch(addMessage(Math.random(), 'error', e))
+      })
+      .finally(() => {
+        setLoading(false)
+      })
   }
 
   useEffect(() => {
@@ -104,40 +112,15 @@ const Checkout = () => {
         <div>
           <AddressViewForm address={address} />
           <h1 className="text-base md:text-xl font-medium mt-4">Заказ</h1>
-          <div className="flex flex-col gap-2 mt-2">
-            {cart.map((item) => {
-              return (
-                <div key={item.id} className="flex gap-2 p-2 h-20 shadow-sm">
-                  <img src={item.image.url} alt={item.name} className="h-full rounded-md" />
-                  <div className="text-sm flex flex-col justify-between overflow-hidden">
-                    <h1 className="font-medium overflow-hidden text-ellipsis whitespace-nowrap">{item.name}</h1>
-                    <span className="overflow-hidden text-ellipsis whitespace-nowrap">Количество: {item.count} шт.</span>
-                    <span className="overflow-hidden text-ellipsis whitespace-nowrap">Сумма: {item.count * item.price} тг.</span>
-                  </div>
-                </div>
-              )
-            })}
-          </div>
-          <div className="flex flex-col gap-2 mt-2">
-            <div className="flex items-center">
-              <span className="shrink-0">Общее количество:</span>
-              <div className="w-full border-dotted border-t-2 mx-2 mt-1"></div>
-              <span className="shrink-0">{cart.reduce((prev, current) => { return prev + current.count }, 0)} шт.</span>
-            </div>
-            <div className="flex items-center">
-              <span className="shrink-0">Сумма к оплате:</span>
-              <div className="w-full border-dotted border-t-2 mx-2 mt-1"></div>
-              <span className="shrink-0">{finalPrice()} тг.</span>
-            </div>
-            <div className="flex mt-2 justify-center sm:justify-end">
-              <Button
-                disabled={!card || isLoading}
-                className="md:text-base w-full max-w-[200px]"
-                onClick={payHandle}
-              >
-                {!card ? 'Выберите карту' : (isLoading ? <Spinner type="medium" /> : `Оплатить ${finalPrice()} тг.`)}
-              </Button>
-            </div>
+          <ViewCart cart={cart} />
+          <div className="flex mt-2 justify-center sm:justify-end">
+            <Button
+              disabled={!card || isLoading}
+              className="md:text-base w-full max-w-[200px]"
+              onClick={payHandle}
+            >
+              {!card ? 'Выберите карту' : (isLoading ? <Spinner type="medium" /> : `Оплатить ${finalPrice()} тг.`)}
+            </Button>
           </div>
         </div>
       )}
--- src/routes/Profile/Addresses/AddressDetails.js
+++ src/routes/Profile/Addresses/AddressDetails.js
@@ -3,21 +3,12 @@ import { useSelector } from 'react-redux'
 import { Navigate, useParams } from 'react-router-dom'
 import { LinkBack } from '../../../components'
 import AddressViewForm from '../../../components/AddressViewForm'
+import { getElementById } from '../../../utils/arrayUtils'
 
 const AddressDetails = () => {
   const { id } = useParams()
   const { addresses } = useSelector(state => state.addresses)
-  const address = getAddressById(id)
-
-  function getAddressById(id) {
-    id = parseInt(id)
-    for (let i = 0; i < addresses.length; i++) {
-      const element = addresses[i];
-      if (element.id === id)
-        return element
-    }
-    return null
-  }
+  const address = getElementById(addresses, id)
 
   if (!address)
     return <Navigate to="/error" />
