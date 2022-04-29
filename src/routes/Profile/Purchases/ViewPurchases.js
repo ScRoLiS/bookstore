54ca571c833e481607d3b969496677ff9ae97929
ScRoLiS <34772324+ScRoLiS@users.noreply.github.com>
2022-04-29 18:00:09 +0600

    add: purchases render

--- src/routes/Profile/Purchases/ViewPurchases.js
+++ src/routes/Profile/Purchases/ViewPurchases.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import { useDispatch, useSelector } from "react-redux"
+import { useSelector } from "react-redux"
+import { Link } from 'react-router-dom'
 
 const ViewPurchases = () => {
 
-  const dispatch = useDispatch()
   const { purchases } = useSelector(state => state.purchases)
 
   if (!purchases.length)
@@ -14,6 +14,28 @@ const ViewPurchases = () => {
         </div>
       </div>
     )
+
+  return (
+    <div className="flex flex-col grow">
+      <div className="flex flex-col gap-2">
+        <div className="purchase-table-row font-medium">
+          <div>Номер заказа</div>
+          <div>Кол-во товаров</div>
+          <div>Статус</div>
+        </div>
+        {purchases.map((item) => {
+          return (
+            <div className="purchase-table-row shadow-sm p-2 rounded-md" key={item.id}>
+              <div>{item.id}</div>
+              <div>{item.purchase.length}</div>
+              <div>{item.status}</div>
+              <div><Link to={`/user/purchases/${item.id}`} className="link border-none text-sky-600 hover:text-sky-200 hover:border-none">Подробнее</Link></div>
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
 }
 
 export default ViewPurchases
